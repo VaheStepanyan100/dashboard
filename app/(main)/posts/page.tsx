8146a96a0b582c109fa72e84983dc1c6ec1fc73a
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-06-12 15:46:59 +0400

    Implement PostsPagination

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,10 +1,12 @@
 import BackButton from "@/components/BackButton";
+import PostsPagination from "@/components/posts/PostsPagination";
 import PostsTable from "@/components/posts/PostsTable";
 
 const PostsPage = () => {
   return ( <>
     <BackButton text="Go Back" link="/" />
     <PostsTable />
+    <PostsPagination />
   </> );
 }
  
