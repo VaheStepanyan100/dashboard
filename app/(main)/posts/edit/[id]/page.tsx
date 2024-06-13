e8be340a873deff9b7688966b0bd463e77ae6de1
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-06-13 23:11:26 +0400

    Create PostEditPage component

---
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,9 @@
+import BackButton from "@/components/BackButton";
+
+const PostEditPage = () => {
+  return ( <>
+      <BackButton text="Back To Posts" link="/posts" />
+  </> );
+}
+ 
+export default PostEditPage;
