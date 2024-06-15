f6827cbdce555acd8010ef666a43615f4d09e312
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-06-16 00:03:57 +0400

    Add dark toggler

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -9,7 +9,7 @@ interface DashboardCardProps {
 
 const DashboardCard = ({title, count, icon}: DashboardCardProps) => {
   return (
-    <Card className="bg-slate-100 dard:bg-slate-800 p-4 pb-0">
+    <Card className="bg-slate-100 dark:bg-slate-800 p-4 pb-0">
       <CardContent className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
         <h3>{title}</h3>
         <div className="flex gap-5 justify-center items-center">
